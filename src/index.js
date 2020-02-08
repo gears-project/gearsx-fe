e6c779e2cc24dab63b3bf66bd4de110bc83f57f0
Michiel Kalkman <michiel@nosuchtype.com>
2020-02-08 18:22:29 +1100

    Add apollo

--- src/index.js
+++ src/index.js
@@ -5,6 +5,28 @@ import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 
+import ApolloClient from 'apollo-boost';
+import { gql } from "apollo-boost";
+
+const client = new ApolloClient({
+  uri: 'http://gearsx.local/graphql',
+  // uri: '/graphql',
+});
+
+
+client
+  .query({
+    query: gql`
+      {
+        projects {
+          id
+          name
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
