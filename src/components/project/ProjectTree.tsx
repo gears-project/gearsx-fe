ca229e0bf0f7558bb9506f80a2613b3fc3383817
Michiel Kalkman <michiel@nosuchtype.com>
2020-04-06 11:23:13 +1000

    feat: use rc-tree for project tree

--- src/components/project/ProjectTree.tsx
+++ src/components/project/ProjectTree.tsx
@@ -10,10 +10,8 @@ import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 import AddIcon from '@material-ui/icons/Add';
 
-import TreeView from '@material-ui/lab/TreeView';
-import TreeItem from '@material-ui/lab/TreeItem';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import Tree, { TreeNode } from 'rc-tree';
+import 'styles/rc-tree.css';
 
 import gql from 'graphql-tag';
 import { useMutation } from '@apollo/react-hooks';
@@ -58,24 +56,30 @@ export default function(props) {
 	}
 
 	const project = props.project;
+
 	const domains = project.model.domains;
 	const xflows = project.model.xflows;
-	// const pages = [];
+  const lookup = {};
+  domains.forEach(doc => {
+			lookup[doc.id] = 'domain';
+  });
+  xflows.forEach(doc => {
+			lookup[doc.id] = 'xflow';
+  });
 
-	const pageLinkFn = (id) => { };
-  const flowLinkFn = (id) => {
+  function goToDoc(id) {
     const projectId = routeMatch.params.projectId;
-    return ()=> {
-      history.push(routes.xflow(projectId, id));
+    switch(lookup[id]) {
+      case 'xflow':
+        history.push(routes.xflow(projectId, id));
+        break;
+      case 'domain':
+        history.push(routes.domain(projectId, id));
+        break;
+      default:
+        console.error('ProjectTree : Unknown lookup type ', lookup[id]);
     }
-  };
-
-  const domainLinkFn = (id) => {
-    const projectId = routeMatch.params.projectId;
-    return ()=> {
-      history.push(routes.domain(projectId, id));
-    }
-  };
+  }
 
   function addDocument(e) {
     e.preventDefault();
@@ -88,19 +92,19 @@ export default function(props) {
     return (res && res.length && res.length === 4);
   }
 
-  function nodeSelected(e, value) {
-    console.log('nodeSelected', value, selectedNode);
-    e.preventDefault();
-    if (isUuid(value)) {
-      if (selectedNode === value) {
-        console.log('nodeSelected 0');
-        setSelectedNode(null);
-      } else {
-        setSelectedNode(value);
-        console.log('nodeSelected 1');
-      }
+  function onSelect(arr, e) {
+    console.log('onSelect', arr, selectedNode);
+    if (arr.length === 0) {
+      setSelectedNode(null);
     } else {
-        console.log('nodeSelected wtf');
+      const value = arr[0];
+      if (isUuid(value)) {
+        setSelectedNode(value);
+        goToDoc(value);
+      } else {
+        console.error('ProjectTree : Invalid identifier received');
+        setSelectedNode(null);
+      }
     }
   }
 
@@ -133,59 +137,32 @@ export default function(props) {
             <AddIcon />
           </IconButton>
         </div>
-        <TreeView
-          className={classes.tree}
-          onNodeSelect={nodeSelected}
-          defaultExpanded={[]}
-          defaultCollapseIcon={<MinusSquare />}
-          defaultExpandIcon={<PlusSquare />}
-          defaultEndIcon={<CloseSquare />}
+
+        <Tree
+          className="myCls"
+          showLine
+          checkable={false}
+          defaultExpandAll
+          onSelect={onSelect}
+          onActiveChange={key => console.log('Active:', key)}
         >
-          <ProjectTreeDocumentList title="Domains" documents={domains} linkFn={domainLinkFn} />
-          <ProjectTreeDocumentList title="Flows" documents={xflows} linkFn={flowLinkFn} />
-        </TreeView>
+          { ProjectTreeDocumentList({title:"Domains", documents:domains}) }
+          { ProjectTreeDocumentList({title:"Flows", documents:xflows}) }
+        </Tree>
       </div>
 	);
 }
-
 function ProjectTreeDocumentList(props) {
   const title = props.title;
   const documents = props.documents;
   const linkFn = props.linkFn;
 
   return (
-    <TreeItem nodeId={title} label={title}>
+    <TreeNode key={title} title={title}>
       {documents.map(({id, name}) => (
-        <TreeItem id={id} nodeId={id} label={name} onClick={linkFn(id)} />
+        <TreeNode key={id} title={name} />
       ))}
-    </TreeItem>
-  );
-}
-
-function MinusSquare(props: SvgIconProps) {
-  return (
-    <SvgIcon fontSize="inherit" style={{ width: 14, height: 14 }} {...props}>
-      {/* tslint:disable-next-line: max-line-length */}
-      <path d="M22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0zM17.873 11.023h-11.826q-.375 0-.669.281t-.294.682v0q0 .401.294 .682t.669.281h11.826q.375 0 .669-.281t.294-.682v0q0-.401-.294-.682t-.669-.281z" />
-    </SvgIcon>
-  );
-}
-
-function PlusSquare(props: SvgIconProps) {
-  return (
-    <SvgIcon fontSize="inherit" style={{ width: 14, height: 14 }} {...props}>
-      {/* tslint:disable-next-line: max-line-length */}
-      <path d="M22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0zM17.873 12.977h-4.923v4.896q0 .401-.281.682t-.682.281v0q-.375 0-.669-.281t-.294-.682v-4.896h-4.923q-.401 0-.682-.294t-.281-.669v0q0-.401.281-.682t.682-.281h4.923v-4.896q0-.401.294-.682t.669-.281v0q.401 0 .682.281t.281.682v4.896h4.923q.401 0 .682.281t.281.682v0q0 .375-.281.669t-.682.294z" />
-    </SvgIcon>
-  );
-}
-
-function CloseSquare(props: SvgIconProps) {
-  return (
-    <SvgIcon className="close" fontSize="inherit" style={{ width: 14, height: 14 }} {...props}>
-      {/* tslint:disable-next-line: max-line-length */}
-      <path d="M17.485 17.512q-.281.281-.682.281t-.696-.268l-4.12-4.147-4.12 4.147q-.294.268-.696.268t-.682-.281-.281-.682.294-.669l4.12-4.147-4.12-4.147q-.294-.268-.294-.669t.281-.682.682-.281.696 .268l4.12 4.147 4.12-4.147q.294-.268.696-.268t.682.281 .281.669-.294.682l-4.12 4.147 4.12 4.147q.294.268 .294.669t-.281.682zM22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0z" />
-    </SvgIcon>
+    </TreeNode>
   );
 }
 
