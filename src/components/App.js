28b8d4a94f027f0da9d395039b9bbe1ae3e409a7
Michiel Kalkman <michiel@nosuchtype.com>
2020-04-07 23:16:28 +1000

    feat: use dark mode based on user preference

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,27 @@
 import React from 'react';
 import '../styles/App.css';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import Dashboard from 'components/Dashboard';
 import { ConfirmProvider } from 'material-ui-confirm';
 
-const darkTheme = createMuiTheme({
-  palette: {
-    type: 'light',
-  },
-});
+export default function() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      message: "Greetings, Yo!"
-    }
-  }
-  render() {
-    return (
-        <ThemeProvider theme={darkTheme}>
-          <ConfirmProvider>
-            <Dashboard />
-          </ConfirmProvider>
-        </ThemeProvider>
-    );
-  }
+  const theme = React.useMemo(() =>
+    createMuiTheme({
+      palette: {
+        type: prefersDarkMode ? 'dark' : 'light',
+      },
+    }),
+    [prefersDarkMode],
+  );
+
+  return (
+    <ThemeProvider theme={theme}>
+      <ConfirmProvider>
+        <Dashboard />
+      </ConfirmProvider>
+    </ThemeProvider>
+  );
 }
-
-export default App;
